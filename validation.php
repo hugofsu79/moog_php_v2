354a5697a7ca61104825541c1faef6af42b77c3e
hugofsu79 <131622641+hugofsu79@users.noreply.github.com>
2023-05-30 12:25:15 +0200

    structure de base ready

---
+++ validation.php
@@ -0,0 +1,22 @@
+<?php
+// inclure le fichier des fonctions pour pouvoir les appeler ici
+include 'function.php';
+
+session_start();
+
+//J'inclus le head avec les balises de base + la balise head(pour ne pas répeter le code qu'il contient)
+include 'head.php';
+?>
+
+    <?php
+    include 'header.php';
+    ?>
+
+    <main>
+    <?php
+    ?>
+    </main>
+
+    <?php
+    include 'footer.php';
+    ?>
